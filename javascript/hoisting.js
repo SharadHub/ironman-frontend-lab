fbca7f7e376697c96b71845c535ad98b04b21ae1
Sharad Bista <bishalbista737@gmail.com>
2025-09-20 23:10:44 +0545

    Hoisting in the javascript variables

---
+++ javascript/hoisting.js
@@ -0,0 +1,19 @@
+// var x = 7;
+
+// function getName(){
+// console.log("Namaste Everyone!!!");
+// }
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+
+
+var n= 2
+function square (num){
+    var ans = num * num;
+    return ans;
+}
+var square2 = square(n);
+var square4 = square(4);
